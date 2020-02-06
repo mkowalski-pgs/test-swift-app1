a5875259d72f64bcf908c7a0a4a69eea39957f0f
Michał Kowalski <mkowalski@pgs-soft.com>
2020-02-06 16:40:55 +0100

    initial

---
+++ test-swift-app1Tests/test_swift_app1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  test_swift_app1Tests.swift
+//  test-swift-app1Tests
+//
+//  Created by Michal Kowalski on 06/02/2020.
+//  Copyright © 2020 PGS Software. All rights reserved.
+//
+
+import XCTest
+@testable import test_swift_app1
+
+class test_swift_app1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
