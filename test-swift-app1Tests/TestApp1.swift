37beccd74f05cd89c23570a2b2004ed281f2049d
Michał Kowalski <mkowalski@pgs-soft.com>
2020-02-07 12:40:11 +0100

    second commit

---
+++ test-swift-app1Tests/TestApp1.swift
@@ -0,0 +1,28 @@
+//
+//  TestApp1.swift
+//  test-swift-app1Tests
+//
+//  Created by Michal Kowalski on 07/02/2020.
+//  Copyright © 2020 PGS Software. All rights reserved.
+//
+
+import XCTest
+
+class TestApp1: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+
+    func testExample2() {
+        XCTAssertTrue(true)
+    }
+}
